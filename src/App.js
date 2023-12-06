3b8a9670e00cb21b9e9ab6eac884454839852ff7
Mardixx <kimi.lefort.pro@gmail.com>
2023-12-06 14:57:11 +0100

    Local Storage BABYYYYYY

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { React } from "react";
 import { ToDos } from "./Todos/todos";
 import "./App.css"
 
 function App() {
   return (
     <div className="App-div">
-      <h1>My Todo App</h1>
+      <h1>My Todos</h1>
       <ToDos />
     </div>
     
--- src/Todos/todos.jsx
+++ src/Todos/todos.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "../Form/form.jsx"
+
+const LSKEY = "Added";
 
 export const ToDos = () => {
     const initialTodos = ["Learn at BeCode", "Be Important!", "Be cool stay safe!"];
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState(
+        JSON.parse(window.localStorage.getItem(LSKEY + ".todos")) || initialTodos
+    );
     function addingTodo(inputIn) {
         const newTodos = [...todos];
         newTodos.push(inputIn);
         setTodos(newTodos);
+        
     }
+    useEffect(() => {
+        window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+    }, [todos]);
+    
+
+
+    
     return(
         <>
-        <div>
+        <div className="form-div">
             <Form addingTodo={addingTodo} />
         </div>
         <div className="view-tasks">
